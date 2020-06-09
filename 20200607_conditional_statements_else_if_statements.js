0e11a03e3b030018653e2b8f206ecbeea817bd4a
austenheringer <62265038+austenheringer@users.noreply.github.com>
2020-06-09 14:43:47 -0600

    Delete 20200607_conditional_statements_else_if_statements.js

--- 20200607_conditional_statements_else_if_statements.js
+++
@@ -1,9 +0,0 @@
-let season = 'summer';
-
-if (season === 'spring') {
-  console.log('It\'s spring! The trees are budding!');
-  } else if (season === 'winter') {
-    console.log('it\'s winter! Everything is covered in snow.')
-  } else {
-  console.log('Invalid season.');
-}
