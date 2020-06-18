647a1fc70e131a1a6cdb62e39362bd067f9f4a22
austenheringer <62265038+austenheringer@users.noreply.github.com>
2020-06-18 10:55:47 -0600

    Add files via upload

--- 20200616/left_rotate_constraint.js
+++ 20200616/left_rotate_constraint.js
@@ -1,6 +1,8 @@
 a = [1, 2, 3, 4, 5]
 let rotLeft = function (a, d) {
-    if (d) a.unshift(a.pop());
-    return a;
-}
-console.log(rotLeft(a, 1));
+    for (let i = 0; i < d; i++) {
+        a.unshift(a.pop());
+    }
+    return a
+};
+console.log(rotLeft(a, 2));
